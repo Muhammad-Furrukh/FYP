d22c1514f8328d34d9e91d3b498f8efec2c44163
SaimaaKhalil <saimakhalil1312@gmail.com>
2026-04-19 20:12:34 +0500

    Updated Issue buffer

--- rtl/lsu_issue_buffer.sv
+++ rtl/lsu_issue_buffer.sv
@@ -205,3 +205,23 @@ module lsu_issue_buffer #(
     end
 
 endmodule
+
+module priority_encoder #(
+    parameter int WIDTH = 8
+)(
+    input  logic [WIDTH-1:0]           req,
+    output logic [$clog2(WIDTH)-1:0]   grant_idx,
+    output logic                       grant_valid
+);
+    always_comb begin
+        grant_idx   = '0;
+        grant_valid = 1'b0;
+        // iterate high to low so lowest index wins  
+        for (int i = WIDTH-1; i >= 0; i--) begin
+            if (req[i]) begin
+                grant_idx   = ($clog2(WIDTH))'(unsigned'(i));
+                grant_valid = 1'b1;
+            end
+        end
+    end
+endmodule 
