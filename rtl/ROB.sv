6f16913098443f61b973feaf71108bae44e9c9a7
Aleeza <aleeza2702@gmail.com>
2026-04-28 16:39:14 +0500

    Modified ROB.sv

--- rtl/ROB.sv
+++ rtl/ROB.sv
@@ -1,132 +1,170 @@
 import include_pkg::*;
-module ROB
+module my_ROB
 (
-    input  logic           clk,
-    input  logic           rst,
-    input  rename_rob_t    rename_rob [RENAME_WIDTH],
-    input  logic           flush,
-    input  sqN_t           flush_sqN,
-    input  logic           CDB_valid  [NUM_CDB_LINES],
-    input  sqN_t           CDB_sqN    [NUM_CDB_LINES],
+input  logic           clk,
+input  logic           rst,
+input  rename_rob_t    rename_rob [RENAME_WIDTH],
+input  logic           flush,
+input  sqN_t           flush_sqN,
+input  logic           CDB_valid  [NUM_CDB_LINES],
+input  sqN_t           CDB_sqN    [NUM_CDB_LINES],
 
-    output logic           OUT_busy,
-    output commit_packet_t OUT_commit [COMMIT_WIDTH]
+output logic           OUT_busy,
+output commit_packet_t OUT_commit [COMMIT_WIDTH]
 );
 
-    logic [COMMIT_WIDTH-1:0] commit;
-    logic [COMMIT_WIDTH-1:0] dataout_valid;
-    logic [RENAME_WIDTH-1:0] write_en;
+rob_entry rob [(ROB_SIZE-1):0];
 
-    logic [$clog2(ROB_SIZE):0]   count, next_count;
-    logic [$clog2(ROB_SIZE)-1:0] head, tail, next_head, next_tail;
+logic [($clog2(ROB_SIZE)):0] count;
+logic [($clog2(ROB_SIZE)-1):0] head, tail;
 
-    rob_entry rob [ROB_SIZE];
+logic write_en [RENAME_WIDTH];
+logic commit [COMMIT_WIDTH];
+logic dataout_valid [COMMIT_WIDTH];
 
-    logic [$clog2(ROB_SIZE)-1:0] alloc_index  [RENAME_WIDTH];
-    logic [$clog2(ROB_SIZE)-1:0] commit_index [COMMIT_WIDTH];
-    logic [$clog2(RENAME_WIDTH):0] num_wr;
-    logic [$clog2(COMMIT_WIDTH):0] num_commit;
+logic full;
+logic empty;
 
-    logic full, empty;
+logic is_new_alloc;
+logic [RENAME_WIDTH] num_write;
+logic [COMMIT_WIDTH] num_commit;
 
-    // ---------------- COMBINATIONAL ----------------
-    always_comb begin
-        logic [$clog2(ROB_SIZE)-1:0] next_head, next_tail;
-        OUT_busy = (count > ROB_SIZE - RENAME_WIDTH);
+logic [COMMIT_WIDTH] c_ptr;
+logic [RENAME_WIDTH] w_ptr;
 
-        full  = (count == 64);
+logic stop_commit;
+logic actual_commit;
 
-        empty = (count == 0);
-
-        for (int i = 0; i < RENAME_WIDTH; i++) begin
-            alloc_index[i] = tail + i;
-            write_en[i]    = rename_rob[i].valid && (!OUT_busy);
-        end
-
-        next_tail = tail;
-        for (int i = 0; i < RENAME_WIDTH; i++) begin
-            next_tail = next_tail + write_en[i];
-        end
-
-        for (int i = 0; i < COMMIT_WIDTH; i++) begin
-            commit_index[i]  = head + i;
-            commit[i]        = rob[commit_index[i]].ready && (!empty);
-            dataout_valid[i] = OUT_commit[i].valid;
-        end
-
-        next_head = head;
-        for (int i = 0; i < COMMIT_WIDTH; i++) begin
-            next_head = next_head + commit[i];
-        end
-
-        next_count = count + (next_tail - tail) - (next_head - head);
-        
+// ---------------- COMBINATIONAL ----------------
+always_comb begin
+    
+    OUT_busy = (count > ROB_SIZE - RENAME_WIDTH);
+    
+    for (int i=0; i<RENAME_WIDTH; i++) begin
+        write_en[i] = (rename_rob[i].valid && (count < (ROB_SIZE-i)));
     end
 
-    // ---------------- SEQUENTIAL ----------------
-    always_ff @(posedge clk or posedge rst) begin
-
+    for (int j=0; j<COMMIT_WIDTH; j++) begin
+        commit[j] = rob[head+j].ready && (!empty);
+    
         if (rst) begin
-            count <= 0;
-            head  <= 0;
-            tail  <= 0;
-            OUT_commit[0].valid <= 0;
-            OUT_commit[1].valid <= 0;
-            
-            for (int i = 0; i < 64; i++) begin
-                rob[i] <= '0;
-            end
+            dataout_valid[j] = 0; 
+        end
+        else begin
+            dataout_valid[j] = OUT_commit[j].valid; 
+        end
+    end
+    
+    num_write  = 0;
+    num_commit = 0;
+
+    for (int i = 0; i < RENAME_WIDTH; i++) begin
+        num_write += (write_en[i] && !full);
+    end
+    for (int i = 0; i < COMMIT_WIDTH; i++) begin
+        num_commit += (commit[i] && !empty && actual_commit);
+    end    
+    
+end
+
+// ---------------- SEQUENTIAL ----------------
+always_ff @(posedge clk or posedge rst) begin
+
+    if (rst) begin
+        count <= 0;
+        head  <= 0;
+        tail  <= 0;
+        actual_commit = 0;
+        c_ptr = 0;
+        w_ptr = 0;
+        stop_commit = 0;
+        is_new_alloc = 0;
+        for (int k=0; k<COMMIT_WIDTH; k++) begin
+            OUT_commit[k].valid <= 0;
+        end
+        
+        for (int l = 0; l < ROB_SIZE; l++) begin
+            rob[l] <= '0;
+        end
+    end
+
+    else if  (flush && !empty) begin
+        for (int a = 0; a < COMMIT_WIDTH; a++) begin
+            OUT_commit[a].valid <= 0;
         end
 
-        else if (flush && !empty) begin
-                OUT_commit[0].valid <= 0;
-                OUT_commit[1].valid <= 0;
-                $display("inside flush big statement");
-                if (rob[tail-1].sqN < flush_sqN) begin
-                    $display("inside formula1");
-                    tail <= tail - (rob[tail-1].sqN-flush_sqN+128);
-                    count <= count - (rob[tail-1].sqN-flush_sqN+128);
-                end
-                else begin
-                    $display("inside formula2");
-                    tail <= tail-(rob[tail-1].sqN-flush_sqN);
-                    count <= count - (rob[tail-1].sqN-flush_sqN);
-                end
-            end
-
+        if (rob[tail-1].SqN < flush_sqN) begin                   
+            tail <= tail - (rob[tail-1].SqN-flush_sqN+(2*ROB_SIZE));
+            count <= count - (rob[tail-1].SqN-flush_sqN+(2*ROB_SIZE));
+        end
         else begin
-            count <= next_count;
-            head  <= next_head;
-            tail  <= next_tail;
-
-            for (int i = 0; i < COMMIT_WIDTH; i++) begin
-                if (commit[i]) begin
-                    OUT_commit[i].sqN     <= rob[commit_index[i]].sqN;
-                    OUT_commit[i].comTag  <= rob[commit_index[i]].tag;
-                    OUT_commit[i].archTag <= rob[commit_index[i]].rd;
-                    OUT_commit[i].valid   <= 1;
-                end
-                else begin
-                    OUT_commit[i].valid <= 0;
-                end
-            end
-
+            tail <= tail-(rob[tail-1].SqN-flush_sqN);
+            count <= count - (rob[tail-1].SqN-flush_sqN);     
+        end
+    end
+    
+    else
+        // ----------------CDB sqN Matching---------------------------
+        for (int j = 0; j < ROB_SIZE; j++) begin
+            is_new_alloc = 0;
             for (int i = 0; i < RENAME_WIDTH; i++) begin
-                if (write_en[i]) begin
-                    rob[alloc_index[i]].sqN   <= rename_rob[i].sqN;
-                    rob[alloc_index[i]].tag   <= rename_rob[i].rd_tag;
-                    rob[alloc_index[i]].rd    <= rename_rob[i].archTag;
-                    rob[alloc_index[i]].ready <= 0;
-                end
+                is_new_alloc |= (j == (tail+i)) && write_en[i];
             end
 
-            for (int i = 0; i < ROB_SIZE; i++) begin
-                for (int j = 0; j < NUM_CDB_LINES; j++) begin
-                    if ((rob[i].sqN == CDB_sqN[j]) && CDB_valid[j]) begin
-                        rob[i].ready <= 1;
+            if (is_new_alloc) begin
+                rob[j].ready <= 0;
+            end
+            else begin
+                for (int k = 0; k < NUM_CDB_LINES; k++) begin
+                    if ((rob[j].SqN == CDB_sqN[k]) && CDB_valid[k]) begin
+                        rob[j].ready <= 1;
                     end
                 end
             end
-        end
+
+        // ----------------ALLOCATE, COMMIT, POINTER UPDATE-------------
+
+            w_ptr = 0;
+            c_ptr = 0;
+            stop_commit = 0;
+            // ------------------READ----------COMMIT-----------
+            for (int i = 0; i < COMMIT_WIDTH; i++) begin
+                if (!stop_commit && (commit[i] && rob[head+c_ptr].ready)) begin
+                    // commit allowed
+                    OUT_commit[c_ptr].sqN     <= rob[head + c_ptr].SqN;
+                    OUT_commit[c_ptr].comTag  <= rob[head + c_ptr].tag;
+                    OUT_commit[c_ptr].archTag <= rob[head + c_ptr].rd;
+                    OUT_commit[c_ptr].valid   <= rob[head + c_ptr].ready;
+                    actual_commit = 1;
+                    c_ptr++;
+                end
+                
+                else begin
+                    // break condition → inorder enforcement
+                    for (int ab = 0; ab < COMMIT_WIDTH; ab++) begin
+                    OUT_commit[ab].valid   <= 0;
+                    end
+                    stop_commit = 1;
+                    actual_commit = 0;
+                end
+            end
+            //--------------WRITE-----------ALLOCATE-----------
+            for (int i = 0; i < RENAME_WIDTH; i++) begin
+                if (write_en[i]) begin
+                    rob[tail + w_ptr].SqN <= rename_rob[i].sqN;
+                    rob[tail + w_ptr].tag <= rename_rob[i].rd_tag;
+                    rob[tail + w_ptr].rd  <= rename_rob[i].archTag;
+                    w_ptr++;
+                end
+            end
+        
+            count <= count + num_write - num_commit;
+            tail  <= tail + num_write;
+            head  <= head + num_commit;
     end
+end
+
+assign full  = (count == ROB_SIZE);
+assign empty = (count == 0);
+
 endmodule
