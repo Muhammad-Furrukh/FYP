c77e001487b11b7098a3e0f65c2338761c460806
Muhammad Furrukh <mfurrukh12082002@gmail.com>
2026-04-28 19:04:14 +0500

    Fixed bugs. Problems remain in dispatch_unit, rename_table, mul_div unit, issue buffers, and flush controller.

--- rtl/ta_gen2.sv
+++ rtl/ta_gen2.sv
@@ -13,41 +13,42 @@ module ta_gen2 (
     output logic            jump2
 );
 
-    logic [$clog2(NUM_ALU_FU)-1:0] oldest_idx;
-    sqN_t oldest_sqN;
+    logic [$clog2(NUM_ALU_FU)-1:0] oldest_idx [NUM_ALU_FU];
 
     always_comb begin
 
-        oldest_idx = '0;
-        oldest_sqN = '1;
-        for (int i = 0; i < NUM_ALU_FU; i++) begin
+        oldest_idx = '{default: '0};
+        for (int i = 1; i < NUM_ALU_FU; i++) begin
             if (IN_valid[i] && 
-            (br_taken[i] || (jump_type[i] == 2'b10)) &&
-            instr_sqN[i] < oldest_sqN) begin
-                oldest_sqN = instr_sqN[i];
-                oldest_idx = i;
-            end
+            (br_taken[i] || (jump_type[i] == JALR)) &&
+            (((oldest_idx[i] - instr_sqN[i]) & SQN_MASK)) < ROB_SIZE)
+                oldest_idx[i] = i;
+            
+            else
+                oldest_idx[i] = oldest_idx[i-1];
         end
 
-        if (IN_valid[oldest_idx]) begin
-            if (br_taken[oldest_idx]) begin
+        if (IN_valid[oldest_idx[NUM_ALU_FU-1]]) begin
+            if (br_taken[oldest_idx[NUM_ALU_FU-1]]) begin
                 jump2 = 1'b1;
-                jta2  = pc[oldest_idx] + 
-                        imm[oldest_idx];
+                jta2  = pc[oldest_idx[NUM_ALU_FU-1]] + 
+                        imm[oldest_idx[NUM_ALU_FU-1]];
             end
 
-            else if (jump_type[oldest_idx] == 2'b10) begin
+            else if (jump_type[oldest_idx[NUM_ALU_FU-1]] == JALR) begin
                 jump2 = 1'b1;
-                jta2  = alu_rs1_result[oldest_idx] + 
-                        imm[oldest_idx];
+                jta2  = alu_rs1_result[oldest_idx[NUM_ALU_FU-1]] + 
+                        imm[oldest_idx[NUM_ALU_FU-1]];
             end
 
             else
                 jump2 = 1'b0;
+                jta2  = '0;
         end
 
         else
             jump2 = 1'b0;
+            jta2  = '0;
     end
 
 endmodule
