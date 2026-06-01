f52a7337f6fe2ec188ab649c9d51fcc0df962df5
AR1Saleh <mrsyed212@gmail.com>
2026-06-01 11:14:30 +0500

    Rebased different commit paths

--- include/include_pkg.sv
+++ include/include_pkg.sv
@@ -15,7 +15,7 @@
 	parameter int unsigned REG_ADDR_WIDTH    = 6;
 	parameter int unsigned DMEM_ADDR_WIDTH   = 10;
 	parameter int unsigned IMEM_ADDR_WIDTH   = 10;
-	parameter int unsigned ROB_SIZE          = 8;
+	parameter int unsigned ROB_SIZE          = 64;
 	parameter int unsigned FETCHB_SIZE       = 16;
 	parameter int unsigned ISSUEB_SIZE       = 8;
 	parameter int unsigned STOREB_SIZE       = 16;
